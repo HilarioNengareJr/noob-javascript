cb0734335880826f1da0bb2a943d986d9e8667b5
hils nengare <hnengare@gmail.com>
2022-10-14 21:38:01 +0200

    committing sw

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('helloworld.html')
+
+@app.route('/variable')
+def var():
+    return render_template('variables.html')
 
 
 if __name__ == '__main__':
